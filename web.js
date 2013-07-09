77da2c66c2992ba14f2bc5cac2523a0f5ec689d6
l337 <autonomousmachine@gmail.com>
2013-07-09 01:37:34 +0000

    adding index, and modifiing web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buffer = new Buffer("Hello World from index.html", 25);
+  var data = fs.readFileSync("index.html", "utf-8");
+  response.send(data);;
 });
 
 var port = process.env.PORT || 5000;
